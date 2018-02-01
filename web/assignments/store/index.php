98bd643c4222736d644c8f6c5f06523b34ec4337
Glaser-Allie <gla13006@byui.edu>
2018-01-31 22:55:49 -0600

    minor text fixes

--- web/assignments/store/index.php
+++ web/assignments/store/index.php
@@ -135,7 +135,9 @@ else if(isset($_GET['view_cart'])) {
 			</form>
 			<div class='checkout'>
 				<a href='./index.php?checkout=1'>Checkout</a>
-			</div>";
+			</div>
+            
+            ";
 	}
 }
 // Checkout
@@ -156,27 +158,55 @@ else if(isset($_GET['checkout'])) {
 					<li class='quantity'>Qty</li>
 				</ul>";
 				
-				$total_candy = 0;
+				$total_price = 0;
 				foreach($_SESSION['shopping_cart'] as $id => $product) {
 					$product_id = $product['product_id'];
 					
 					
 					$total_price += $products[$product_id]['price'] * $product['quantity'];
-					echo "<ul>
+					echo "           
+                    <ul>
                     <li class='name'><a href='./index.php?view_product=$id'>" . 
 							$products[$product_id]['name'] . "</a></li>
                             <li class='price' >$" . $products[$product_id]['price'] . "</li> 
 						<li class='quantity'>" . $product['quantity'] . "</li>
-						<li class='calc_price'>$" . ($products[$product_id]['price'] * $product['quantity']) . "</li>
+						<li class='calc_price'>$" . number_format(($products[$product_id]['price'] * $product['quantity']),2) . "</li>
                         </ul>
 						
 					";
 				}
 			echo "
             <ul>
-			<li class='total'>Total price:\n$" . $total_price . "<li>
-            </ul>";
-		
+			<li class='total'>Total price:\n\n\n</li><li class='final_total'>$" . number_format($total_price,2) . "<li>
+            </ul>
+            
+            <div class='checkout_info'>
+            <p>Please enter your shipping information:</p>
+
+			<input type='text' placeholder='Name' name='name'>
+			<br/>
+            <br/>
+
+			<input type='text' placeholder='Email Address' name='email'>
+			<br/>
+            <br/>
+            
+			<input type='text' placeholder='Street Address' name='address'>
+			<br/>
+            <br/>
+            
+            
+            <div class='citystatezip'>
+            <input type='text' placeholder='City' name='city'>\n\n 
+            <input type='text' placeholder='State' name='state' maxlength='2'>\n\n\n\n
+            <input type='text' placeholder='Zip' name='zip' maxlength='5'>
+            </div>
+            
+			<br />
+            <input type='submit' value='Complete Purchase'>
+            </div>";
+            
+     
 	}
 }
 // View all products
@@ -189,10 +219,15 @@ else {
 	// Loop to display all products
 	foreach($products as $id => $product) {
 		echo "<div class='pokecard'>
-			<div><h4><a href='./index.php?view_product=$id'>" . $product['name'] . "</h4></a></div>
-            <div>" . $product['image'] . "</div>
+			<div><h4><a href='./index.php?view_product=$id'>" . $product['name'] . "</h4></div>
+            <div>" . $product['image'] . "</div></a>
 			<div class='card_description'>" . $product['type'] . "\n\n|\n\n" . $product['wave'] . "\n Wave</div>
             <div>$" . $product['price'] . "</div>
+            <form action='./index.php?view_product=$product_id' method='post'>
+					<input type='hidden' name='product_id' value='$product_id' />
+					<input type='submit' name='add_to_cart' value='Add to cart' />
+				</form>
+            
 		</div>";
 	}
 }
